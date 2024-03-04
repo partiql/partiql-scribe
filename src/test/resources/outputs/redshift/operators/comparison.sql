8c427ab1af13e7ca75e6f8c121abd5d24e042fb8
R. C. Howell <howero@amazon.com>
2024-03-04 11:58:03 -0800

    Address PR feedback

--- src/test/resources/outputs/redshift/operators/comparison.sql
+++ src/test/resources/outputs/redshift/operators/comparison.sql
@@ -13,7 +13,7 @@ SELECT "T"."c" = "T"."z" AS "_1" FROM "default"."T" AS "T";
 --#[eq-04]
 SELECT "T"."c" = "T"."d"."e" AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[eq-05]
 SELECT "T"."c" = "T"."x" AS "_1" FROM "default"."T" AS "T";
 
@@ -32,35 +32,35 @@ SELECT "T"."c" <> "T"."z" AS "_1" FROM "default"."T" AS "T";
 --#[neq-04]
 SELECT "T"."c" <> "T"."d"."e" AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[neq-05]
 SELECT "T"."c" <> "T"."x" AS "_1" FROM "default"."T" AS "T";
 
 --#[lt-00]
 SELECT "T"."b" < 1 AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[lt-01]
 SELECT "T"."b" < "T"."x" AS "_1" FROM "default"."T" AS "T";
 
 --#[lte-00]
 SELECT "T"."b" <= 1 AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[lte-01]
 SELECT "T"."b" <= "T"."x" AS "_1" FROM "default"."T" AS "T";
 
 --#[gt-00]
 SELECT "T"."b" > 1 AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[gt-01]
 SELECT "T"."b" > "T"."x" AS "_1" FROM "default"."T" AS "T";
 
 --#[gte-00]
 SELECT "T"."b" >= 1 AS "_1" FROM "default"."T" AS "T";
 
--- DISABLED !! "T.".x" is type ANY which is not supported by Trino
+-- DISABLED !! "T.".x" is type ANY which is not supported by Redshift
 -- #[gte-01]
 -- SELECT "T"."b" >= "T"."x" AS "_1" FROM "default"."T" AS "T";
 
