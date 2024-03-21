068d8c77cd66bcc77974f0e15086f4e18e633edd
Alan Cai <caialan@amazon.com>
2024-03-21 15:41:53 -0700

    Fix failing tests from previous commit

--- src/test/resources/outputs/partiql/basics/simple.sql
+++ src/test/resources/outputs/partiql/basics/simple.sql
@@ -41,7 +41,7 @@ true;
 "default"."T1" AND "default"."T1";
 
 --#[expr-02]
-NOT "default"."T1";
+NOT ("default"."T1");
 
 --#[expr-03]
 "default"."T1" < "default"."T1";
@@ -68,139 +68,139 @@ NOT "default"."T1";
 "default"."T1" IS NULL;
 
 --#[expr-11]
-NOT "default"."T1" IS NULL;
+NOT ("default"."T1" IS NULL);
 
 --#[expr-12]
 "default"."T1" IS MISSING;
 
 --#[expr-13]
-NOT "default"."T1" IS MISSING;
+NOT ("default"."T1" IS MISSING);
 
 --#[expr-14]
 "default"."T1" IS INT2;
 
 --#[expr-15]
-NOT "default"."T1" IS INT2;
+NOT ("default"."T1" IS INT2);
 
 --#[expr-16]
 "default"."T1" IS INT4;
 
 --#[expr-17]
-NOT "default"."T1" IS INT4;
+NOT ("default"."T1" IS INT4);
 
 --#[expr-18]
 "default"."T1" IS INT8;
 
 --#[expr-19]
-NOT "default"."T1" IS INT8;
+NOT ("default"."T1" IS INT8);
 
 --#[expr-20]
 "default"."T1" IS INT;
 
 --#[expr-21]
-NOT "default"."T1" IS INT;
+NOT ("default"."T1" IS INT);
 
 --#[expr-22]
 "default"."T1" IS DECIMAL;
 
 --#[expr-23]
-NOT "default"."T1" IS DECIMAL;
+NOT ("default"."T1" IS DECIMAL);
 
 --#[expr-24]
 "default"."T1" IS DOUBLE PRECISION;
 
 --#[expr-25]
-NOT "default"."T1" IS DOUBLE PRECISION;
+NOT ("default"."T1" IS DOUBLE PRECISION);
 
 --#[expr-26]
 "default"."T1" IS BOOL;
 
 --#[expr-27]
-NOT "default"."T1" IS BOOL;
+NOT ("default"."T1" IS BOOL);
 
 --#[expr-28]
 "default"."T1" IS SYMBOL;
 
 --#[expr-29]
-NOT "default"."T1" IS SYMBOL;
+NOT ("default"."T1" IS SYMBOL);
 
 --#[expr-30]
 "default"."T1" IS DATE;
 
 --#[expr-31]
-NOT "default"."T1" IS DATE;
+NOT ("default"."T1" IS DATE);
 
 --#[expr-32]
 "default"."T1" IS TIME;
 
 --#[expr-33]
-NOT "default"."T1" IS TIME;
+NOT ("default"."T1" IS TIME);
 
 --#[expr-34]
 "default"."T1" IS TIMESTAMP;
 
 --#[expr-35]
-NOT "default"."T1" IS TIMESTAMP;
+NOT ("default"."T1" IS TIMESTAMP);
 
 --#[expr-36]
 "default"."T1" IS STRING;
 
 --#[expr-37]
-NOT "default"."T1" IS STRING;
+NOT ("default"."T1" IS STRING);
 
 --#[expr-38]
 "default"."T1" IS CLOB;
 
 --#[expr-39]
-NOT "default"."T1" IS CLOB;
+NOT ("default"."T1" IS CLOB);
 
 --#[expr-40]
 "default"."T1" IS BLOB;
 
 --#[expr-41]
-NOT "default"."T1" IS BLOB;
+NOT ("default"."T1" IS BLOB);
 
 --#[expr-42]
 "default"."T1" IS LIST;
 
 --#[expr-43]
-NOT "default"."T1" IS LIST;
+NOT ("default"."T1" IS LIST);
 
 --#[expr-44]
 "default"."T1" IS SEXP;
 
 --#[expr-45]
-NOT "default"."T1" IS SEXP;
+NOT ("default"."T1" IS SEXP);
 
 --#[expr-46]
 "default"."T1" IS STRUCT;
 
 --#[expr-47]
-NOT "default"."T1" IS STRUCT;
+NOT ("default"."T1" IS STRUCT);
 
 --#[expr-48]
 "default"."T1" IS BAG;
 
 --#[expr-49]
-NOT "default"."T1" IS BAG;
+NOT ("default"."T1" IS BAG);
 
 --#[expr-50]
 "default"."T1" IN (true);
 
 --#[expr-51]
-NOT "default"."T1" IN (false);
+NOT ("default"."T1" IN (false));
 
 --#[expr-52]
 "default"."T1" IN "default"."T1";
 
 --#[expr-53]
-NOT "default"."T1" IN "default"."T1";
+NOT ("default"."T1" IN "default"."T1");
 
 --#[expr-54]
 "default"."T1" LIKE "default"."T1";
 
 --#[expr-55]
-NOT "default"."T1" LIKE "default"."T1";
+NOT ("default"."T1" LIKE "default"."T1");
 
 --#[expr-56]
 "default"."T1" LIKE "default"."T1" ESCAPE "default"."T1";
@@ -209,7 +209,7 @@ NOT "default"."T1" LIKE "default"."T1";
 "default"."T1" BETWEEN "default"."T1" AND "default"."T1";
 
 --#[expr-58]
-NOT "default"."T1" BETWEEN "default"."T1" AND "default"."T1";
+NOT ("default"."T1" BETWEEN "default"."T1" AND "default"."T1");
 
 --#[expr-59]
 "default"."T1" || "default"."T1";
