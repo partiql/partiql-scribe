9627955dacad7e878d4391f6037462e2d2cb5feb
Alan Cai <caialan@amazon.com>
2024-06-06 08:01:35 -0700

    Handle simple EXCLUDE top-level column case in Redshift; expand out SELECT * (#49)

--- src/test/resources/outputs/redshift/basics/select.sql
+++ src/test/resources/outputs/redshift/basics/select.sql
@@ -2,7 +2,8 @@
 SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c" FROM "default"."T" AS "T";
 
 --#[select-01]
-SELECT "T".* FROM "default"."T" AS "T";
+-- expand out the `SELECT *`
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T";
 
 -- #[select-02]
 -- ERROR, no SELECT VALUE!
@@ -13,7 +14,7 @@ SELECT "T".* FROM "default"."T" AS "T";
 -- SELECT VALUE a FROM "default"."T" AS "T";
 
 --#[select-04]
-SELECT "t1".*, "t2".* FROM "default"."T" AS "t1" INNER JOIN "default"."T" AS "t2" ON true;
+SELECT "t1"."a" AS "a", "t1"."b" AS "b", "t1"."c" AS "c", "t1"."d" AS "d", "t1"."x" AS "x", "t1"."array" AS "array", "t1"."z" AS "z", "t1"."v" AS "v", "t1"."timestamp_1" AS "timestamp_1", "t1"."timestamp_2" AS "timestamp_2", "t2"."a" AS "a", "t2"."b" AS "b", "t2"."c" AS "c", "t2"."d" AS "d", "t2"."x" AS "x", "t2"."array" AS "array", "t2"."z" AS "z", "t2"."v" AS "v", "t2"."timestamp_1" AS "timestamp_1", "t2"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "t1" INNER JOIN "default"."T" AS "t2" ON true;
 
 -- Redshift doesn't support struct wildcard (i.e. <SUPER OBJECT>.*). Rewriting to include every struct field.
 --#[select-05]
@@ -29,7 +30,7 @@ SELECT "T"."d"."e" AS "e", "T"."d"."e" AS "e" FROM "default"."T" AS "T";
 SELECT "T"."d"."e" AS "e" FROM "default"."T" AS "T";
 
 --#[select-09]
-SELECT "T".* FROM "default"."T" AS "T";
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T";
 
 --#[select-10]
 SELECT "T"."c" || CURRENT_USER AS "_1" FROM "default"."T" AS "T";
@@ -47,49 +48,49 @@ SELECT "t"."a" AS "a" FROM "default"."T" AS "t";
 -- SELECT VALUE {z: a} FROM T;
 
 --#[select-14]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" BETWEEN 0 AND 2;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" BETWEEN 0 AND 2;
 
 --#[select-15]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" NOT BETWEEN 0 AND 2;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" NOT BETWEEN 0 AND 2;
 
 --#[select-16]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" NOT BETWEEN 0 AND 2;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" NOT BETWEEN 0 AND 2;
 
 --#[select-17]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" BETWEEN 0 AND 2;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" BETWEEN 0 AND 2;
 
 --#[select-18]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IN (0, 1, 2);
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IN (0, 1, 2);
 
 --#[select-19]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" NOT IN (0, 1, 2);
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" NOT IN (0, 1, 2);
 
 --#[select-20]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" NOT IN (0, 1, 2);
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" NOT IN (0, 1, 2);
 
 --#[select-21]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IN (0, 1, 2);
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IN (0, 1, 2);
 
 --#[select-22]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IS NULL;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IS NULL;
 
 --#[select-23]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IS NOT NULL;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IS NOT NULL;
 
 --#[select-24]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IS NOT NULL;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IS NOT NULL;
 
 --#[select-25]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."b" IS NULL;
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."b" IS NULL;
 
 --#[select-26]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."c" LIKE 'abc';
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."c" LIKE 'abc';
 
 --#[select-27]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."c" NOT LIKE 'abc';
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."c" NOT LIKE 'abc';
 
 --#[select-28]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."c" NOT LIKE 'abc';
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."c" NOT LIKE 'abc';
 
 --#[select-29]
-SELECT "T".* FROM "default"."T" AS "T" WHERE "T"."c" LIKE 'abc';
+SELECT "T"."a" AS "a", "T"."b" AS "b", "T"."c" AS "c", "T"."d" AS "d", "T"."x" AS "x", "T"."array" AS "array", "T"."z" AS "z", "T"."v" AS "v", "T"."timestamp_1" AS "timestamp_1", "T"."timestamp_2" AS "timestamp_2" FROM "default"."T" AS "T" WHERE "T"."c" LIKE 'abc';
