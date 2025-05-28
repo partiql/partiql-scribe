e32eb7f049ec276ea26d096d90bceecf790019d2
Alan Cai <caialan@amazon.com>
2025-05-28 16:26:08 -0700

    feat: PartiQL -> Trino transpilation (other than EXCLUDE) (#92)

--- src/main/kotlin/org/partiql/scribe/targets/trino/TrinoAstToSql.kt
+++ src/main/kotlin/org/partiql/scribe/targets/trino/TrinoAstToSql.kt
@@ -1,6 +1,119 @@
 package org.partiql.scribe.targets.trino
 
+import org.partiql.ast.Ast.exprCast
+import org.partiql.ast.Ast.exprLit
+import org.partiql.ast.Ast.exprPathStepField
+import org.partiql.ast.DataType
+import org.partiql.ast.Identifier
+import org.partiql.ast.Literal
+import org.partiql.ast.expr.ExprBag
+import org.partiql.ast.expr.ExprCall
+import org.partiql.ast.expr.ExprLit
+import org.partiql.ast.expr.ExprSessionAttribute
+import org.partiql.ast.expr.PathStep
+import org.partiql.ast.sql.SqlBlock
 import org.partiql.scribe.ScribeContext
+import org.partiql.scribe.problems.ScribeProblem
 import org.partiql.scribe.sql.AstToSql
+import org.partiql.scribe.sql.utils.concat
+import org.partiql.scribe.sql.utils.list
+import java.math.BigDecimal
 
-public open class TrinoAstToSql(context: ScribeContext) : AstToSql(context)
+public open class TrinoAstToSql(context: ScribeContext) : AstToSql(context) {
+    private val listener = context.getProblemListener()
+
+    override fun visitExprSessionAttribute(
+        node: ExprSessionAttribute,
+        tail: SqlBlock,
+    ): SqlBlock {
+        return tail concat node.sessionAttribute.name().lowercase()
+    }
+
+    /**
+     * Trino does not support x['y'] syntax; replace with x.y.
+     *
+     * @param node
+     * @param tail
+     * @return
+     */
+    override fun visitPathStepElement(
+        node: PathStep.Element,
+        tail: SqlBlock,
+    ): SqlBlock {
+        val key = node.element
+        return if (key is ExprLit && key.lit.code() == Literal.STRING) {
+            listener.report(
+                ScribeProblem.simpleInfo(
+                    code = ScribeProblem.TRANSLATION_INFO,
+                    message =
+                        "Trino does not support PartiQL's path element syntax (e.g. x['y']). " +
+                            "Replaced with path step field syntax (e.g. x.y)",
+                ),
+            )
+            val elemString = key.lit.stringValue()
+            val stepField = exprPathStepField(Identifier.Simple.delimited(elemString))
+            visitPathStepField(stepField, tail)
+        } else {
+            super.visitPathStepElement(node, tail)
+        }
+    }
+
+    override fun visitExprLit(
+        node: ExprLit,
+        tail: SqlBlock,
+    ): SqlBlock {
+        val v = node.lit
+        if (v.code() == Literal.INT_NUM && intValueOutOfRange(v.bigDecimalValue())) {
+            // CAST('<v>' AS DECIMAL(38,0))
+            val lit = Literal.string(v.bigDecimalValue().toString())
+            val ast = exprCast(exprLit(lit), DataType.DECIMAL(38, 0))
+            return visitExprCast(ast, tail)
+        }
+        return super.visitExprLit(node, tail)
+    }
+
+    private fun intValueOutOfRange(value: BigDecimal): Boolean {
+        return value < Long.MIN_VALUE.toBigDecimal() || Long.MAX_VALUE.toBigDecimal() < value
+    }
+
+    override fun visitExprCall(
+        node: ExprCall,
+        tail: SqlBlock,
+    ): SqlBlock {
+        var t = tail
+        val f = node.function
+        // Special case -- DATE_ADD('<datetime_field>', <lhs>, <rhs>) -> DATE_ADD(<datetime_field>, <lhs>, <rhs>)
+        // Special case -- DATE_DIFF('<datetime_field>', <lhs>, <rhs>) -> DATE_DIFF(<datetime_field>, <lhs>, <rhs>)
+        if (!f.hasQualifier() &&
+            (f.identifier.text.uppercase() == "DATE_ADD" || f.identifier.text.uppercase() == "DATE_DIFF") &&
+            node.args.size == 3
+        ) {
+            val start = "("
+            t = visitIdentifier(f, t)
+            t = t concat list(this, start) { node.args }
+            return t
+        }
+        return super.visitExprCall(node, tail)
+    }
+
+    override fun visitDataType(
+        node: DataType,
+        tail: SqlBlock,
+    ): SqlBlock {
+        return when (node.code()) {
+            DataType.INT2 -> tail concat "SMALLINT"
+            DataType.INT4 -> tail concat "INT"
+            DataType.INT8 -> tail concat "BIGINT"
+            DataType.DOUBLE_PRECISION -> tail concat "DOUBLE"
+            DataType.STRING -> tail concat "VARCHAR"
+            else -> super.visitDataType(node, tail)
+        }
+    }
+
+    override fun visitExprBag(
+        node: ExprBag,
+        tail: SqlBlock,
+    ): SqlBlock {
+        return tail concat list(this, "(", ")") { node.values }
+    }
+}
