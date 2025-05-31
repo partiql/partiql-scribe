32deacf73460cd4d54d9c5179d45bec8bab7be67
Alan Cai <caialan@amazon.com>
2025-05-30 17:48:48 -0700

    feat: ORDER BY plan to ast translation

---
+++ src/test/resources/inputs/basics/order-by.sql
@@ -0,0 +1,26 @@
+--#[order-by-00]
+SELECT a FROM T ORDER BY T.a;
+
+--#[order-by-01]
+SELECT a FROM T ORDER BY T.a, T.b;
+
+--#[order-by-02]
+SELECT a FROM T ORDER BY a, b;
+
+--#[order-by-03]
+SELECT a FROM T ORDER BY T.a ASC, T.b DESC;
+
+--#[order-by-04]
+SELECT a FROM T ORDER BY T.a ASC NULLS FIRST, T.b DESC NULLS LAST;
+
+--#[order-by-05]
+SELECT a FROM T ORDER BY T.a ASC NULLS LAST, T.b DESC NULLS FIRST;
+
+--#[order-by-06]
+(SELECT a FROM T ORDER BY T.a, T.b) UNION (SELECT a FROM T ORDER BY T.a, T.b);
+
+--#[order-by-07]
+(SELECT a, b FROM T ORDER BY T.a, T.b) UNION (SELECT a, b FROM T ORDER BY T.a, T.b) ORDER BY a;
+
+--#[order-by-08]
+(SELECT a, b FROM T ORDER BY T.a, T.b LIMIT 1 OFFSET 2) UNION (SELECT a, b FROM T ORDER BY T.a, T.b LIMIT 3 OFFSET 4) ORDER BY a LIMIT 5 OFFSET 6;
