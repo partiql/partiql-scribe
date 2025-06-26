cd3d1f6ff4e4523424c0e36ce5715eb1890aac34
Alan Cai <caialan@amazon.com>
2025-06-26 11:52:40 -0700

    [0.x] feat: typed casts for decimal

--- src/test/resources/outputs/partiql/operators/cast.sql
+++ src/test/resources/outputs/partiql/operators/cast.sql
@@ -15,3 +15,13 @@ SELECT CAST('1' AS BIGINT) AS "_1" FROM "default"."T" AS "T";
 
 --#[cast-05]
 SELECT CAST(1 AS DOUBLE PRECISION) AS "_1" FROM "default"."T" AS "T";
+
+--#[cast-06]
+SELECT CAST(1 AS DECIMAL) AS "res" FROM "default"."T" AS "T";
+
+-- 0 scale is provided by PLK 0.14.9's AST -> Plan conversion
+--#[cast-07]
+SELECT CAST(1 AS DECIMAL(5,0)) AS "res" FROM "default"."T" AS "T";
+
+--#[cast-08]
+SELECT CAST(1 AS DECIMAL(5,2)) AS "res" FROM "default"."T" AS "T";
