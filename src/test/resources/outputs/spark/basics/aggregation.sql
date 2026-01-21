c2292b7eb0322a42c47f60694d78efd062ce40a8
Di Xiao <diwxiao@amazon.com>
2026-01-21 15:37:14 -0800

    add test cases to aggregate time and timestamp

--- src/test/resources/outputs/spark/basics/aggregation.sql
+++ src/test/resources/outputs/spark/basics/aggregation.sql
@@ -15,3 +15,21 @@ SELECT `max`(`T`.`b`) AS `_1` FROM `default`.`T` AS `T`;
 
 --#[rel-aggregation-max-01]
 SELECT `max`(`T`.`b`) AS `_1` FROM `default`.`T` AS `T` GROUP BY `T`.`a`;
+
+--#[rel-aggregation-max-min-date]
+SELECT `max`(`T`.`col_date`) AS `_1`, `min`(`T`.`col_date`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
+
+--#[rel-aggregation-max-min-time]
+SELECT `max`(`T`.`col_time`) AS `_1`, `min`(`T`.`col_time`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
+
+--#[rel-aggregation-max-min-timez]
+SELECT `max`(`T`.`col_timez`) AS `_1`, `min`(`T`.`col_timez`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
+
+--#[rel-aggregation-max-min-timestamp]
+SELECT `max`(`T`.`col_timestamp`) AS `_1`, `min`(`T`.`col_timestamp`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
+
+--#[rel-aggregation-max-min-timestampz]
+SELECT `max`(`T`.`col_timestampz`) AS `_1`, `min`(`T`.`col_timestampz`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
+
+--#[rel-aggregation-max-min-string]
+SELECT `max`(`T`.`col_string`) AS `_1`, `min`(`T`.`col_string`) AS `_2` FROM `default`.`T_ALL_TYPES` AS `T`;
