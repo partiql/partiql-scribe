3cc37de7d66578ccf6fead1ad1542a87457b9644
xd1313113 <diwxiao@amazon.com>
2026-01-26 11:10:37 -0800

    add test cases to aggregate time and timestamp (#137)

--- src/test/resources/inputs/basics/aggregation.sql
+++ src/test/resources/inputs/basics/aggregation.sql
@@ -15,3 +15,21 @@ SELECT MAX(b) FROM T;
 
 --#[rel-aggregation-max-01]
 SELECT MAX(b) FROM T GROUP BY a;
+
+--#[rel-aggregation-max-min-date]
+SELECT MAX(col_date), MIN(col_date) FROM T_ALL_TYPES AS T;
+
+--#[rel-aggregation-max-min-time]
+SELECT MAX(col_time), MIN(col_time) FROM T_ALL_TYPES AS T;
+
+--#[rel-aggregation-max-min-timez]
+SELECT MAX(col_timez), MIN(col_timez) FROM T_ALL_TYPES AS T;
+
+--#[rel-aggregation-max-min-timestamp]
+SELECT MAX(col_timestamp), MIN(col_timestamp) FROM T_ALL_TYPES AS T;
+
+--#[rel-aggregation-max-min-timestampz]
+SELECT MAX(col_timestampz), MIN(col_timestampz) FROM T_ALL_TYPES AS T;
+
+--#[rel-aggregation-max-min-string]
+SELECT MAX(col_string), MIN(col_string) FROM T_ALL_TYPES AS T;
